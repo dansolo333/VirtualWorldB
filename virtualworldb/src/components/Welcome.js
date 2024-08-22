2c527f310d3c0bc041b4e68bd53c7eb128ceee72
dansolo333 <danielosigbodi@gmail.com>
2024-08-22 03:05:38 +0300

    deploy

--- virtualworldb/src/components/Welcome.js
+++ virtualworldb/src/components/Welcome.js
@@ -6,8 +6,7 @@ function Welcome() {
   return (
     <div className="welcome-container">
       <h1>Welcome to VIRTUAL WORLD B!</h1>
-      <p>Please log in to continue.</p>
-      <Link className="link" to="/login">Login</Link>
+      <p>Please <Link className="link" to="/login">Login</Link> to continue or <Link className="link" to="/login">Login</Link>.</p>
     </div>
   );
 }
