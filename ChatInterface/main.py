ac3a30451a801856f6971920fe878a1d5d3f05ec
dansolo333 <danielosigbodi@gmail.com>
2024-08-22 20:27:54 +0300

    changed port to 10000 in main.py and procfile

--- ChatInterface/main.py
+++ ChatInterface/main.py
@@ -250,4 +250,4 @@ def smart_contract_send_money(sender: str, recipient: str, amount: float) -> boo
 # To run the FastAPI app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
