2c2925c55069d0bca8f6453168f118eb6bb3cd61
dansolo333 <danielosigbodi@gmail.com>
2024-08-23 00:48:33 +0300

    changed server url

--- virtualworldb/src/components/SignUp.js
+++ virtualworldb/src/components/SignUp.js
@@ -10,7 +10,7 @@ function SignUp() {
 
   const handleSignUp = async () => {
     try {
-      const response = await fetch('https://virtualworldb.onrender.com/signup', {
+      const response = await fetch('https://virtualworldb-server.onrender.com/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
